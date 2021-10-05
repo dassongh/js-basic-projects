10b16929b39f72f9cb592be0d43faceb5572d691
Oleksandr S <oleksandr.slv@gmail.com>
2021-10-05 19:49:34 +0300

    adds more projects

--- 06-modal/setup/app.js
+++ 06-modal/setup/app.js
@@ -2,3 +2,21 @@
 // listen for click events on modal-btn and close-btn
 // when user clicks modal-btn add .open-modal to modal-overlay
 // when user clicks close-btn remove .open-modal from modal-overlay
+
+const refs = {
+  modalBtn: document.querySelector('.modal-btn'),
+  closeBtn: document.querySelector('.close-btn'),
+  modalOverlay: document.querySelector('.modal-overlay'),
+};
+
+refs.modalBtn.addEventListener('click', toggleModal);
+
+refs.closeBtn.addEventListener('click', toggleModal);
+
+refs.modalOverlay.addEventListener('click', (e) => {
+  if (e.target === e.currentTarget) toggleModal();
+})
+
+function toggleModal() {
+  refs.modalOverlay.classList.toggle('open-modal');
+}
--- 07-questions/setup/app.js
+++ 07-questions/setup/app.js
@@ -1,2 +1,25 @@
-//using selectors inside the element
+// using selectors inside the element
 // traversing the dom
+
+const questions = document.querySelectorAll('.question');
+
+questions.forEach((question) => {
+  const btn = question.querySelector('.question-btn');
+
+  btn.addEventListener('click', () => {
+    questions.forEach((el) => {
+      if (el !== question) el.classList.remove('show-text');
+    });
+    
+    question.classList.toggle('show-text');
+  });
+});
+
+
+// const btnsRef = document.querySelectorAll('.question-btn');
+
+// btnsRef.forEach((el) => {
+//   el.addEventListener('click', (e) => {
+//     e.currentTarget.parentElement.parentElement.classList.toggle('show-text');
+//   });
+// });
